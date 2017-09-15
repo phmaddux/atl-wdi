fe4fedf076abc0efb7ad25ab52c7f5b627c46563
Hunter Maddux <phmaddux@gmail.com>
2017-09-14 20:59:19 -0400

    added card images

--- projects/unit_01/Blackjack/Deck.js
+++ projects/unit_01/Blackjack/Deck.js
@@ -4,7 +4,7 @@
     }
     createDeck(){
         var deck = [];
-        var values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+        var values = ["aces", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
         var suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
         values.forEach(function(value) {
             suits.forEach(function(suit) {
@@ -26,22 +26,46 @@
 
 }
 
-var deck = new Deck();
-deck.getCard(); 
-
-// shuffle function
-
-// deal function
-
-// hit & stand in jquery
-
-// scoring
-
-// reset
 
 
 
+// pull a random card
+
+// more stuff here
+// var deck = new Deck();
+// deck.getCard(); 
+
+// // deal (random) function, does deal go before or after the score function?
+
+// var randomCard = deck[Math.floor(Math.random() * deck.length)];
+// console.log(randomCard)
+// pop i global variable
+// // +1?
+// // hit & stand in jquery
+// set up flex box append child
 
 
 
+        if (playerScore > 21) {
+            message = "Bust. Player loses...";
+        }
+        else if (dealerScore > 21) {
+            message = "Dealer busts! Player wins!";
+        }
+        else if (playerScore <= 21 && playerHand.length >= 5) {
+            message = "Player wins!";
+        }
+        else if (playerScore === dealerScore) {
+            message = "Tie game.";
+        }
+        else if (playerScore > dealerScore) {
+            message = "Player wins!";
+        }
+        else if (playerScore < dealerScore) {
+            message = "Player loses.";
+        }
+        return message;
+    
 
+
+// Rocks fall, everyone dies ~ reset
