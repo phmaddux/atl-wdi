f878b362cb75c9e724e9ad3f68d827588fc58881
Hunter Maddux <phmaddux@gmail.com>
2017-10-20 13:20:11 -0400

    Commit: NFL - wow there's a lot of nfl data

--- homework/sql/nfl/nfl.sql
+++ homework/sql/nfl/nfl.sql
@@ -1,5 +1,10 @@
-3)
-3.1)SELECT * FROM teams;
-3.2)SELECT stadium, head_coach FROM teams;
-3.3)SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
-3.4)SELECT COUNT(*) FROM players;
+
+1)SELECT * FROM teams;
+2)SELECT stadium, head_coach FROM teams;
+3)SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
+4)SELECT COUNT(*) FROM players;
+
+5)SELECT name, head_coach FROM teams WHERE conference ='NFC' AND division ='North' OR conference='AFC' AND division='East';
+6)SELECT name FROM players ORDER BY salary DESC LIMIT 50;
+7)SELECT AVG(salary) FROM players;
+8)SELECT name, position FROM players  WHERE salary > 10000000;
