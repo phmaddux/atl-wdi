9797729c0c14d4101972fe611f858b24e261b140
Hunter Maddux <phmaddux@gmail.com>
2017-10-20 13:27:14 -0400

    Commit: NFL - Finished the nfl queries

--- homework/sql/nfl/nfl.sql
+++ homework/sql/nfl/nfl.sql
@@ -8,3 +8,10 @@
 6)SELECT name FROM players ORDER BY salary DESC LIMIT 50;
 7)SELECT AVG(salary) FROM players;
 8)SELECT name, position FROM players  WHERE salary > 10000000;
+
+9)SELECT name FROM players ORDER BY salary DESC LIMIT 1;
+10)SELECT name, position, salary FROM players  WHERE salary > 10000000 ORDER BY salary ASC;
+11)SELECT AVG(salary) FROM players WHERE position='DE';
+12)SELECT players.name, teams.name FROM players, teams WHERE players.team_id=teams.id AND teams.name LIKE 'Buffalo Bills'
+13)SELECT AVG(players.salary) FROM players, teams WHERE players.team_id=teams.id AND teams.name LIKE 'New York Giants';
+14) (I am unsure how to do this one)
