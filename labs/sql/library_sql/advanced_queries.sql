1c0720682c7a270a1d8f71117dcd3b2529e1da51
Hunter Maddux <phmaddux@gmail.com>
2017-10-20 12:59:44 -0400

    NFL - I seeded the database with the players and teams info

--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,10 +2,26 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT name, title
+FROM authors
+JOIN books ON authors.id = books.author_id
+WHERE name = 'George R.R. Martin';
+
+SELECT * FROM authors
+JOIN books ON authors.id = books.author_id
+WHERE UPPER(authors.name) = UPPER('George R.R. Martin')
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+SELECT name, title
+FROM authors
+JOIN books ON authors.id = books.author_id
+WHERE name = 'Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
+
+SELECT * FROM authors
+INNER JOIN authors ON books.author_id = authors.id
+WHERE authors.nationality = 'China OR United Kingdom';
 
 -- Find out how many books Albert Camus wrote.
 
