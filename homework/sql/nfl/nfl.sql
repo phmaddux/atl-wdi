8b02040e94472fb945ce62c5ce3ddf63a2587110
Hunter Maddux <phmaddux@gmail.com>
2017-10-20 13:16:08 -0400

    Commit: NFL - queried some NFL stuff

---
+++ homework/sql/nfl/nfl.sql
@@ -0,0 +1,5 @@
+3)
+3.1)SELECT * FROM teams;
+3.2)SELECT stadium, head_coach FROM teams;
+3.3)SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
+3.4)SELECT COUNT(*) FROM players;
