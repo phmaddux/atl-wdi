295500fc67e0dbab540a80f73dfff6ae1d893598
Hunter Maddux <phmaddux@gmail.com>
2017-10-23 22:27:51 -0400

    1. Nil Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,6 +1,19 @@
+# Boolean Skills:
+
 a = true
 b = false
 
 puts a == b 
 
 puts a != b
+
+# Nil Skills:
+
+n = nil
+
+puts n.to_s + " was here."
+# Not sure how to get that to show up...
+
+puts n.nil?
+
+puts n.class
