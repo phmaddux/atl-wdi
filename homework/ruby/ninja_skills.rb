8dc60fc96904e238da22a07ae941b3f82e8536da
Hunter Maddux <phmaddux@gmail.com>
2017-10-23 22:55:34 -0400

    4 Strings Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -31,3 +31,21 @@ puts e
 d = e
 
 puts d
+
+# Strings Skills:
+
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda."
+tims = " and I like "
+puts christine << tims << likesTo
+
+g = "2"
+gToNumber = 2
+
+puts g + gToNumber
+# results in
+# ninja_skills.rb:45:in `+': no implicit conversion of Integer into String (TypeError)
+# from ninja_skills.rb:45:in `<main>'
+
+# Strings and Integers cannot be added together properly. I would have to convert the string to an integer. Probably using .oct
+
