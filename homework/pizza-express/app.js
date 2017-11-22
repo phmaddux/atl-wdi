20cf5d3ff669fbc0e525b92adb9bf2f52bec5f0e
Hunter Maddux <phmaddux@gmail.com>
2017-11-22 09:30:32 -0500

    git routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,7 +1,20 @@
 var express = require('express');
-var app     = express();
-var port    = 3000;
+var app = express();
+var port = 3000;
 
-app.listen(port, function(){
+
+
+
+app.get('/', function (req, res, next) {
+  res.send("Welcome to Pizza Express!");
+});
+app.get('/topping/:type', function(req, res, next) {
+  res.send("something pizza good choice!")
+});
+app.get('/order/:amount/:size', function (req, res, next) {
+  res.send (something)
+});
+
+app.listen(port, function () {
   console.log('LISTENING ON PORT ' + port);
 });
